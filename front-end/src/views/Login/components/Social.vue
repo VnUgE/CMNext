64e92373b4156270a52ef882af52b7618a3a26ef
vnugent <public@vaughnnugent.com>
2026-01-20 11:32:47 -0500

    feat(frontend): Display errors in the UI for social login methods that have server side errors

--- front-end/src/views/Login/components/Social.vue
+++ front-end/src/views/Login/components/Social.vue
@@ -28,6 +28,8 @@ const submitLogin = (method: SocialOAuthMethod) => apiCall(async () => {
 
 <template>
 
+    <hr v-show="methods.length > 0" class="my-6 border-base-content" />
+
     <ul v-for="method in enabledMethods" :key="method.method_id" class="">
         <li class="my-2">
             <button type="submit" class="btn flex flex-row w-full" :disabled="waiting"
@@ -39,5 +41,22 @@ const submitLogin = (method: SocialOAuthMethod) => apiCall(async () => {
             </button>
         </li>
     </ul>
+
+    <div v-if="errorMethods.length" class="collapse collapse-arrow border border-base-300 rounded-lg mt-4">
+        <input type="checkbox" />
+        <div class="collapse-title font-medium text-sm">
+            Failed to load {{ errorMethods.length }} method(s)
+        </div>
+        <div class="collapse-content">
+            <ul class="space-y-2">
+                <li v-for="method in errorMethods" :key="method.method_id" class="text-sm p-2 bg-base-200 rounded">
+                    <div class="font-semibold text-base-content">{{ method.data.friendly_name }}</div>
+                    <div class="text-error text-xs mt-2 font-medium">
+                        {{ method.data.error }}
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
 
 </template>
